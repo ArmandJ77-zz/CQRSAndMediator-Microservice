59d04b4d3532424db856354b7f0369d85828bd9e
armand.jordaan@leadhome.co.za <armand.jordaan@leadhome.co.za>
2020-01-08 20:39:57 +0200

    Clean up

--- Microservice.Api/Startup.cs
+++ Microservice.Api/Startup.cs
@@ -24,7 +24,6 @@ namespace Microservice.Api
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddCors(options => options.AddPolicy("Default", builder =>
@@ -57,7 +56,6 @@ namespace Microservice.Api
             services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
