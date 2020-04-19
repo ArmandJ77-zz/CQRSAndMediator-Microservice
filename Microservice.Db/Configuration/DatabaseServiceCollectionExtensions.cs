a2820c61332d7e39bfeb0b408a06a762d60e88fd
armandjordaan@live.co.za <armandjordaan@live.co.za>
2020-04-19 11:24:34 +0200

    Create DatabaseServiceCollectionExtensions.cs

---
+++ Microservice.Db/Configuration/DatabaseServiceCollectionExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Microservice.Db.Configuration
+{
+    public static class DatabaseServiceCollectionExtensions
+    {
+        public static IServiceCollection AddDatabase(this IServiceCollection services, string dbConnectionString)
+        {
+            IServiceProvider serviceProvider = services.BuildServiceProvider();
+            var env = serviceProvider.GetService<IHostingEnvironment>();
+            
+            if (env.IsEnvironment("IntegrationTesting"))
+            {
+                services.AddEntityFrameworkInMemoryDatabase();
+                services.AddDbContext<MicroserviceDbContext>(options =>
+                {
+                    options.UseInMemoryDatabase("InMemoryTestDB");
+                });
+            }
+            else
+            {
+                services.AddDbContext<MicroserviceDbContext>(o => o.UseNpgsql(dbConnectionString));
+
+            }
+
+            return services;
+        }
+    }
+}
