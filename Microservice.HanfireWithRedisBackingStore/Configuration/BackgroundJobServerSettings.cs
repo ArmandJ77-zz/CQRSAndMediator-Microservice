397b17d486b1127cea05f64cb95928e91ae39740
armandjordaan@live.co.za <armandjordaan@live.co.za>
2020-04-19 11:25:17 +0200

    Create BackgroundJobServerSettings.cs

---
+++ Microservice.HanfireWithRedisBackingStore/Configuration/BackgroundJobServerSettings.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Microservice.HanfireWithRedisBackingStore.Configuration
+{
+    public class BackgroundJobServerSettings
+    {
+        public string RedisConnectionString { get; set; }
+        public int RedisDatabaseNumber { get; set; }
+        public int? NumberOfWorkers { get; set; }
+        public int? RetryAttempts { get; set; }
+        public List<string> Queues { get; set; }
+        public string DashboardEndpoint { get; set; }
+    }
+}
