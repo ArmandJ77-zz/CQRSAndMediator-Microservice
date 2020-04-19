3aecab86577a585748102fcea69b39de12f640af
armandjordaan@live.co.za <armandjordaan@live.co.za>
2020-04-19 14:15:13 +0200

    Remove inmemory implementation

--- Microservice.Db/Configuration/DatabaseServiceCollectionExtensions.cs
+++ Microservice.Db/Configuration/DatabaseServiceCollectionExtensions.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Microservice.Db.Configuration
@@ -9,23 +7,7 @@ namespace Microservice.Db.Configuration
     {
         public static IServiceCollection AddDatabase(this IServiceCollection services, string dbConnectionString)
         {
-            IServiceProvider serviceProvider = services.BuildServiceProvider();
-            var env = serviceProvider.GetService<IHostingEnvironment>();
-            
-            if (env.IsEnvironment("IntegrationTesting"))
-            {
-                services.AddEntityFrameworkInMemoryDatabase();
-                services.AddDbContext<MicroserviceDbContext>(options =>
-                {
-                    options.UseInMemoryDatabase("InMemoryTestDB");
-                });
-            }
-            else
-            {
-                services.AddDbContext<MicroserviceDbContext>(o => o.UseNpgsql(dbConnectionString));
-
-            }
-
+            services.AddDbContext<MicroserviceDbContext>(o => o.UseNpgsql(dbConnectionString));
             return services;
         }
     }
