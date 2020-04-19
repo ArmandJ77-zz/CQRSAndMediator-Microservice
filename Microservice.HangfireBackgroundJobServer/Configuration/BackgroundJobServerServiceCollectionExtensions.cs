ba8c58a987d3911eecd45b57ee67f67b58c750c8
armandjordaan@live.co.za <armandjordaan@live.co.za>
2020-04-19 14:15:18 +0200

    Update BackgroundJobServerServiceCollectionExtensions.cs

--- Microservice.HangfireBackgroundJobServer/Configuration/BackgroundJobServerServiceCollectionExtensions.cs
+++ Microservice.HangfireBackgroundJobServer/Configuration/BackgroundJobServerServiceCollectionExtensions.cs
@@ -1,12 +1,14 @@
 ﻿using Hangfire;
 using Hangfire.Dashboard;
+using Hangfire.PostgreSql;
 using Microservice.HanfireWithRedisBackingStore.Infrastructure;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
+using System;
 using System.Collections.Generic;
-using Hangfire.PostgreSql;
 
 namespace Microservice.HanfireWithRedisBackingStore.Configuration
 {
