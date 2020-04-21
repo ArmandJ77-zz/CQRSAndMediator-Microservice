e09daa880b1f92d1007ad2531302dfb3178994c4
armandjordaan@live.co.za <armandjordaan@live.co.za>
2020-04-21 07:30:52 +0200

    Update IRabbitMessageBrokerClient.cs

--- Microservice.RabbitMessageBroker/IRabbitMessageBrokerClient.cs
+++ Microservice.RabbitMessageBroker/IRabbitMessageBrokerClient.cs
@@ -7,17 +7,19 @@ namespace Microservice.RabbitMessageBroker
     public interface IRabbitMessageBrokerClient
     {
         Task<Action> Subscribe<T>(
-            string topic, 
-            string subscriptionId, 
+            string topic,
+            string subscriptionId,
             Func<T, Task> onReceive,
-            Func<MessageBrokerConfigBuilder, MessageBrokerConfigBuilder> configure = null);
+            Func<MessageBrokerConfigBuilder, MessageBrokerConfigBuilder> configure = null
+            );
 
         Task<Action> Subscribe(
             string topic,
             string subscriptionId,
             Func<object, Task> onReceive,
             Type eventModelType,
-            Func<MessageBrokerConfigBuilder, MessageBrokerConfigBuilder> config = null);
+            Func<MessageBrokerConfigBuilder, MessageBrokerConfigBuilder> configure = null
+            );
 
         Task<bool> Publish<T>(string topic, T message);
 
