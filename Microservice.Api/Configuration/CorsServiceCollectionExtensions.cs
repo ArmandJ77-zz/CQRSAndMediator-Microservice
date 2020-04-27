69e43e09e7e6320f95725ca89061e6eb6cee43f5
armandjordaan@live.co.za <armandjordaan@live.co.za>
2020-04-27 16:09:31 +0200

    Create CorsServiceCollectionExtensions.cs

---
+++ Microservice.Api/Configuration/CorsServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Microservice.Api.Configuration
+{
+    public static class CorsServiceCollectionExtensions
+    {
+        public static IServiceCollection AddCorsRules(this IServiceCollection services)
+        {
+            services.AddCors(options => options.AddPolicy("Default", builder =>
+            {
+                builder
+                    .AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader();
+            }));
+
+            return services;
+        }
+    }
+}
